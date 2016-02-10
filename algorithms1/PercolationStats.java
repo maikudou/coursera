15de0e8e56d6e7c308f8d9bbf7db5f1d310c8ad7
Mikhail Labanov <maikudou@gmail.com>
2016-02-10 19:38:53 +0300

    Week 1 assignment

---
+++ algorithms1/PercolationStats.java
@@ -0,0 +1,41 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class PercolationStats {
+    private Percolation perc;
+    private double[] results;
+    private double result;
+    private int times;
+    public PercolationStats(int N, int T){
+        results = new double[T];
+        times = T;
+        for(int i=0; i<T; i++){
+            perc = new Percolation(N);
+            while(!perc.percolates()){
+                perc.open(StdRandom.uniform(1, N+1), StdRandom.uniform(1, N+1));
+            }
+            results[i] = perc.count()/(double)(N*N);
+        }
+
+        System.out.printf("mean                     = %f%n", mean());
+        System.out.printf("stdev                    = %f%n", stddev());
+        System.out.printf("95%% confidence interval  = %f, %f%n", confidenceLo(), confidenceHi());
+    }
+    public double mean(){
+        return StdStats.mean(results);
+    }
+    public double stddev(){
+        return StdStats.stddev(results);
+    }
+    public double confidenceLo(){
+        return mean()-1.96*stddev()/java.lang.Math.sqrt(times);
+    }
+    public double confidenceHi(){
+        return mean()+1.96*stddev()/java.lang.Math.sqrt(times);
+    }
+
+    public static void main(String[] args){
+        PercolationStats percStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+    }
+}
--- algorithms1/percolation.java
+++ algorithms1/percolation.java
@@ -1,32 +1,81 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-static int size;
-static WeightedQuickUnionUF uf;
-
 public class Percolation {
+    private int size;
+    private WeightedQuickUnionUF uf;
+    private boolean[] sites;
+    private int cIndex;
+    private int openCount;
+
     public Percolation(int N){
         if(N <= 0){
             throw new java.lang.IllegalArgumentException();
         }
         size = N;
+        openCount = 0;
         uf = new WeightedQuickUnionUF(N*N);
+        sites = new boolean[N*N];
+        for(int i=0; i < sites.length; i++){
+            sites[i] = false;
+        }
     }
-    public void open(int i, int j)
-    public boolean isOpen(int i, int j)
-    public boolean isFull(int i, int j)
-    public boolean percolates()
+    public void open(int i, int j){
+        if(!isOpen(i, j)){
+            openCount++;
+            cIndex = (i-1)*size+j-1;
+            sites[cIndex]=true;
 
-    public static void main(String[] args)
-}
+            if(j>1){
+                if(isOpen(i, j-1)){
+                    uf.union(cIndex, cIndex-1);
+                }
+            }
+            if(j<size){
+                if(isOpen(i, j+1)){
+                    uf.union(cIndex, cIndex+1);
+                }
+            }
 
-public class PercolationStats {
-    public PercolationStats(int N, int T)
-    public double mean()
-    public double stddev()
-    public double confidenceLo()
-    public double confidenceHi()
+            if(i>1){
+                if(isOpen(i-1, j)){
+                    uf.union(cIndex, (i-2)*size+j-1);
+                }
+            }
+            if(i<size){
+                if(isOpen(i+1, j)){
+                    uf.union(cIndex, (i)*size+j-1);
+                }
+            }
+        }
+    }
+    public boolean isOpen(int i, int j){
+        return sites[(i-1)*size+j-1];
+    }
+    public boolean isFull(int i, int j){
+        int column = 1;
+        while(column <= size){
+            if(isOpen(1, column) && uf.connected(column-1, (i-1)*size+j-1)){
+                return true;
+            }
+            column++;
+        }
+        return false;
+    }
+    public boolean percolates(){
+        int column = 1;
+        while(column <= size){
+            if(isFull(size, column)){
+                return true;
+            }
+            column++;
+        }
+        return false;
+    }
+    public int count(){
+        return openCount;
+    }
 
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+    }
 }
